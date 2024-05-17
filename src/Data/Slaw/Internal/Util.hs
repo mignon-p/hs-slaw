98198d58f113d933aba5376bc3796b7ff3b90dae
Mignon Pelletier <code@funwithsoftware.org>
2024-05-16 19:16:44 -0700

    Re-export float2Double and double2Float from Util.hs

--- src/Data/Slaw/Internal/Util.hs
+++ src/Data/Slaw/Internal/Util.hs
@@ -1,9 +1,12 @@
 module Data.Slaw.Internal.Util
   ( (##)
   , (??)
+  , float2Double -- re-export
+  , double2Float -- re-export
   ) where
 
 import Data.Hashable
+import GHC.Float
 
 infixl 0 ##
 infix  7 ??
