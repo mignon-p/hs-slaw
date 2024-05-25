fc9b19696b6445ca84dd27516573bdda7b5e59fc
Mignon Pelletier <code@funwithsoftware.org>
2024-05-25 09:41:33 -0700

    New file: NativeInt.hs

---
+++ src/Data/Slaw/Internal/NativeInt.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE CPP                        #-}
+
+module Data.Slaw.Internal.NativeInt
+  ( NativeWord
+  , NativeInt
+  , toNativeWord
+  , fromNativeWord
+  , toNativeInt
+  , fromNativeInt
+  ) where
+
+import Data.Int
+import Data.Word
+
+#include "MachDeps.h"
+
+#if WORD_SIZE_IN_BITS > 32
+type NativeWord = Word64
+type NativeInt  = Int64
+#else
+type NativeWord = Word32
+type NativeInt  = Int32
+#endif
+
+{-# INLINE toNativeWord #-}
+toNativeWord :: Integral a => a -> NativeWord
+toNativeWord = fromIntegral
+
+{-# INLINE fromNativeWord #-}
+fromNativeWord :: Integral a => NativeWord -> a
+fromNativeWord = fromIntegral
+
+{-# INLINE toNativeInt #-}
+toNativeInt :: Integral a => a -> NativeInt
+toNativeInt = fromIntegral
+
+{-# INLINE fromNativeInt #-}
+fromNativeInt :: Integral a => NativeInt -> a
+fromNativeInt = fromIntegral
