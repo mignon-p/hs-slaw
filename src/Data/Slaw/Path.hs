a14c9f60dd09d0d944e8046edad6b24f8b4a62e6
Mignon Pelletier <code@funwithsoftware.org>
2024-06-06 20:08:12 -0700

    Expose new module Data.Slaw.Path

---
+++ src/Data/Slaw/Path.hs
@@ -0,0 +1,12 @@
+module Data.Slaw.Path
+  ( slawPath
+  , slawPath_m
+  , slawPath_es
+  , slawPath_ee
+  , (!)
+  , (!?)
+  , (?>)
+  ) where
+
+import Data.Slaw.Internal.SlawPath
+import Data.Slaw.Internal.Util
