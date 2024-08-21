a5cb14a509c992002d5450d12ebf60d3ad92b6fb
Mignon Pelletier <code@funwithsoftware.org>
2024-08-21 15:05:02 -0700

    New Maybe-based records.

--- src/Data/Slaw/Internal/OptionRecords.hs
+++ src/Data/Slaw/Internal/OptionRecords.hs
@@ -12,59 +12,98 @@ Portability : GHC
 module Data.Slaw.Internal.OptionRecords
   ( WriteYamlOptions(..)
   , WriteBinaryOptions(..)
-  , getFileFormat
   , PoolCreateOptions(..)
   ) where
 
 import Control.DeepSeq
--- import qualified Data.ByteString.Short    as SBS
 import Data.Default.Class
 import Data.Hashable
--- import Data.Int
--- import Data.Maybe
--- import qualified Data.Text                as T
--- import qualified Data.Vector.Storable     as S
--- import Data.Word
--- import Foreign.Storable
 import GHC.Generics (Generic)
-import Numeric.Natural
-import System.Info (os)
 
--- import Data.Slaw.Internal.EnumStrings
--- import Data.Slaw.Internal.Exception
 import Data.Slaw.Internal.Nameable
 import Data.Slaw.Internal.OptionTypes
 import Data.Slaw.Internal.SlawConvert
--- import Data.Slaw.Internal.SlawPath
 import Data.Slaw.Internal.SlawType
--- import Data.Slaw.Internal.String
--- import Data.Slaw.Internal.Util
 
 #define FIELD(qzName, qzField) opt qzName qzField \
   (\qzRec qzVal -> qzRec { qzField = qzVal })
 
-#define CFELD(qzName, qzField, qzTo, qzFrom) opt1 qzName qzField \
-  (\qzRec qzVal -> qzRec { qzField = qzVal }) qzTo qzFrom
+#define NFELD(qzName, qzField, qzPref) optN qzName qzField \
+  (\qzRec qzVal -> qzRec { qzField = qzVal }) qzPref
 
 --
 
 data WriteYamlOptions = WriteYamlOptions
-  { wyoTagNumbers       :: !Bool
-  , wyoDirectives       :: !Bool
-  , wyoOrderedMaps      :: !Bool
-  , wyoComment          :: !Bool
-  , wyoMaxArrayElements :: !(Maybe Natural)
-  , wyoAutoFlush        :: !AutoFlush
-  } deriving (Eq, Ord, Show, Read, Generic, NFData, Hashable)
+  { -- | Set it to false if you don't need full fidelity, and would
+    -- rather not have all your numbers tagged.
+    --
+    -- [key]: tag_numbers
+    --
+    -- [type]: boolean
+    --
+    -- [default]: true
+    wyoTagNumbers       :: Maybe Bool
+    -- | Set it to false if you don't want to emit the @%YAML@ and
+    -- @%TAG@ directives.  This makes the tag name more verbose,
+    -- and the YAML version unspecified.
+    --
+    -- [key]: directives
+    --
+    -- [type]: boolean
+    --
+    -- [default]: true
+  , wyoDirectives       :: Maybe Bool
+    -- | If true, encodes slaw maps as Yaml's @!!omap@.  If false,
+    -- encodes slaw maps as Yaml's @!!map@ (which means the
+    -- ordering might be lost).
+    --
+    -- [key]: ordered_maps
+    --
+    -- [type]: boolean
+    --
+    -- [default]: true
+  , wyoOrderedMaps      :: Maybe Bool
+    -- | If true, starts the file with a comment which includes the
+    -- g-speak and libYaml version numbers, and a mode comment to
+    -- prevent emacs from going into idlwave-mode for files with a
+    -- .pro extension.
+    --
+    -- [key]: comment
+    --
+    -- [type]: boolean
+    --
+    -- [default]: true for the file-based functions, but false for the string-based functions
+  , wyoComment          :: Maybe Bool
+    -- | If not -1, arrays will be truncated after this many
+    -- elements are printed.  This makes the file
+    -- non-round-trippable.
+    --
+    -- [key]: max-array-elements
+    --
+    -- [type]: int64
+    --
+    -- [default]: -1
+  , wyoMaxArrayElements :: Maybe Integer
+    -- | Whether to automatically flush the stream whenever a slaw
+    -- is written.  Defaults to only do so if the stream is not
+    -- seekable (and therefore likely a pipe, socket, or terminal).
+    --
+    -- [key]: auto-flush
+    --
+    -- [type]: string - one of “never”, “always”, or “if-not-seekable”.
+    --
+    -- [default]: if-not-seekable
+  , wyoAutoFlush        :: Maybe AutoFlush
+} deriving (Eq, Ord, Show, Read, Generic, NFData, Hashable)
 
 instance Default WriteYamlOptions where
   def = WriteYamlOptions
-        { wyoTagNumbers       = True
-        , wyoDirectives       = True
-        , wyoOrderedMaps      = True
-        , wyoComment          = False
+        { wyoTagNumbers       = Nothing
+        , wyoDirectives       = Nothing
+        , wyoOrderedMaps      = Nothing
+        , wyoComment          = Nothing
         , wyoMaxArrayElements = Nothing
-        , wyoAutoFlush        = def
+        , wyoAutoFlush        = Nothing
         }
 
 instance Nameable WriteYamlOptions where
@@ -74,32 +113,17 @@ instance FromSlaw WriteYamlOptions where
   fromSlaw = Right . recordFromMap writeYamlOptions
 
 instance ToSlaw WriteYamlOptions where
-  toSlaw wyo = SlawMap (p1 ++ p2)
-    where
-      p1 = recordToPairs writeFileOptions $ mkWfo YamlFile
-      p2 = recordToPairs writeYamlOptions wyo
+  toSlaw = recordToMapWithFmt writeYamlOptions YamlFile
 
 writeYamlOptions :: Options WriteYamlOptions
 writeYamlOptions =
-  [ FIELD("tag_numbers",        wyoTagNumbers)
-  , FIELD("directives",         wyoDirectives)
-  , FIELD("ordered_maps",       wyoOrderedMaps)
-  , FIELD("comment",            wyoComment)
-  , CFELD("max-array-elements", wyoMaxArrayElements, tm64, fm64)
-  , FIELD("auto-flush",         wyoAutoFlush)
+  [ FIELD("tag_numbers",        wyoTagNumbers      )
+  , FIELD("directives",         wyoDirectives      )
+  , FIELD("ordered_maps",       wyoOrderedMaps     )
+  , FIELD("comment",            wyoComment         )
+  , NFELD("max-array-elements", wyoMaxArrayElements, NumInt64)
+  , FIELD("auto-flush",         wyoAutoFlush       )
   ]
-
-tm64 :: Maybe Natural -> Slaw
-tm64 Nothing  = preferNumeric NumInt64 (-1)
-tm64 (Just n) = preferNumeric NumInt64 $ toInteger n
-
-fm64 :: Slaw -> Maybe (Maybe Natural)
-fm64 SlawNil = return Nothing
-fm64 s       = do
-  n <- ŝm s
-  if n < 0
-    then return Nothing
-    else return $ Just $ fromInteger n
 
 --
 
@@ -107,14 +131,32 @@ fm64 s       = do
 -- 'Data.Slaw.IO.openBinarySlawOutput' or
 -- 'Data.Slaw.IO.writeBinarySlawFile', to specify options.
 data WriteBinaryOptions = WriteBinaryOptions
-  { wboByteOrder :: !PreferredByteOrder
-  , wboAutoFlush :: !AutoFlush
-  } deriving (Eq, Ord, Show, Read, Generic, NFData, Hashable)
+  { -- | Byte order to write the binary file in.  Doesn't really
+    -- matter, because it will be automatically swapped on read if
+    -- necessary.
+    --
+    -- [key]: byte-order
+    --
+    -- [type]: string - one of “little-endian”, “big-endian”, or “native”.
+    --
+    -- [default]: native
+    wboByteOrder :: Maybe PreferredByteOrder
+    -- | Whether to automatically flush the stream whenever a slaw
+    -- is written.  Defaults to only do so if the stream is not
+    -- seekable (and therefore likely a pipe, socket, or terminal).
+    --
+    -- [key]: auto-flush
+    --
+    -- [type]: string - one of “never”, “always”, or “if-not-seekable”.
+    --
+    -- [default]: if-not-seekable
+  , wboAutoFlush :: Maybe AutoFlush
+} deriving (Eq, Ord, Show, Read, Generic, NFData, Hashable)
 
 instance Default WriteBinaryOptions where
   def = WriteBinaryOptions
-        { wboByteOrder = def
-        , wboAutoFlush = def
+        { wboByteOrder = Nothing
+        , wboAutoFlush = Nothing
         }
 
 instance Nameable WriteBinaryOptions where
@@ -124,10 +166,7 @@ instance FromSlaw WriteBinaryOptions where
   fromSlaw = Right . recordFromMap writeBinaryOptions
 
 instance ToSlaw WriteBinaryOptions where
-  toSlaw wbo = SlawMap (p1 ++ p2)
-    where
-      p1 = recordToPairs writeFileOptions $ mkWfo BinaryFile
-      p2 = recordToPairs writeBinaryOptions wbo
+  toSlaw = recordToMapWithFmt writeBinaryOptions BinaryFile
 
 writeBinaryOptions :: Options WriteBinaryOptions
 writeBinaryOptions =
@@ -137,70 +176,183 @@ writeBinaryOptions =
 
 --
 
-newtype WriteFileOptions = WriteFileOptions
-  { wfoFileFormat :: Maybe FileFormat
-  } deriving (Eq, Ord, Show, Read, Generic, NFData, Hashable)
-
-instance Default WriteFileOptions where
-  def = WriteFileOptions Nothing
-
-instance Nameable WriteFileOptions where
-  typeName _ = "WriteFileOptions"
-
-instance FromSlaw WriteFileOptions where
-  fromSlaw = Right . recordFromMap writeFileOptions
-
-instance ToSlaw WriteFileOptions where
-  toSlaw = recordToMap writeFileOptions
-
-writeFileOptions :: Options WriteFileOptions
-writeFileOptions =
-  [ FIELD("format", wfoFileFormat)
-  ]
-
-mkWfo :: FileFormat -> WriteFileOptions
-mkWfo = WriteFileOptions . Just
-
-getFileFormat :: Slaw -> Maybe FileFormat
-getFileFormat s = ŝm s >>= wfoFileFormat
-
---
-
 data PoolCreateOptions = PoolCreateOptions
-  { pcoResizable    :: !Bool
-  , pcoSingleFile   :: !Bool
-  , pcoSize         :: !Natural
-  , pcoTocCapacity  :: !Natural
-  , pcoStopWhenFull :: !Bool
-  , pcoFrozen       :: !Bool
-  , pcoAutoDispose  :: !Bool
-  , pcoSync         :: !Bool
-  , pcoFlock        :: !Bool
-  , pcoChecksum     :: !Bool
-  , pcoMode         :: !StrNumNone
-  , pcoOwner        :: !StrNumNone
-  , pcoGroup        :: !StrNumNone
-  }
+  { -- | This enables a newer pool format, which supports a number
+    -- of new features, including being able to resize the pool
+    -- after it is created.  You should almost always have this
+    -- enabled.  The only reason to ever disable it is if you want
+    -- to create pools that can be read by very old versions of
+    -- Plasma (g-speak < v2.0).
+    --
+    -- [key]: resizable
+    --
+    -- [type]: boolean
+    --
+    -- [default]: true
+    pcoResizable    :: Maybe Bool
+    -- | This means that the pool will reside in a single file,
+    -- rather than creating an entire directory for the pool.
+    --
+    -- [key]: single-file
+    --
+    -- [type]: boolean
+    --
+    -- [default]: false
+  , pcoSingleFile   :: Maybe Bool
+    -- | The size of the pool, in bytes.  This includes the space
+    -- used for the header and table of contents, so the actual
+    -- amount of space available for proteins will be less.  The
+    -- pool wraps around and deletes older proteins once this limit
+    -- has been exceeded.
+    --
+    -- [key]: size
+    --
+    -- [type]: unt64 (bytes)
+    --
+    -- [default]: (required)
+    --
+    -- [note]: available to 'pool_change_options'
+  , pcoSize         :: Maybe Integer
+    -- | The size of the table of contents, in entries (one entry
+    -- for each protein).  The default value, 0, indicates that
+    -- there should be no table of contents.  The table of contents
+    -- is an optimization which makes random access of proteins
+    -- within the pool faster.  If the number of proteins in the
+    -- pool exceeds the table of contents size, then the table of
+    -- contents is “decimated” so that it records only ever
+    -- other protein, or every 4th protein, etc.
+    --
+    -- [key]: toc-capacity
+    --
+    -- [type]: unt64 (proteins)
+    --
+    -- [default]: 0
+  , pcoTocCapacity  :: Maybe Integer
+    -- | If true, then the pool stops allowing deposits once it is
+    -- full.  If false, the default, it will wrap around and delete
+    -- the oldest proteins as newer ones are deposited.
+    --
+    -- [key]: stop-when-full
+    --
+    -- [type]: boolean
+    --
+    -- [default]: false
+    --
+    -- [note]: available to 'pool_change_options'
+  , pcoStopWhenFull :: Maybe Bool
+    -- | Makes the pool read-only until “frozen” is set to
+    -- false again.
+    --
+    -- [key]: frozen
+    --
+    -- [type]: boolean
+    --
+    -- [default]: false
+    --
+    -- [note]: available to 'pool_change_options'
+  , pcoFrozen       :: Maybe Bool
+    -- | Causes the pool to be automatically deleted once the last
+    -- hose to it is closed.  This option can only be set with
+    -- 'pool_change_options'; it cannot be set when the pool is
+    -- created.
+    --
+    -- [key]: auto-dispose
+    --
+    -- [type]: boolean
+    --
+    -- [default]: false
+    --
+    -- [note]: ONLY available to 'pool_change_options'
+  , pcoAutoDispose  :: Maybe Bool
+    -- | Causes the pool to be synced to disk after every deposit.
+    -- This should eliminate the possiblity of corruption due to
+    -- power failures, at the expense of performance.  Note that on
+    -- Linux, the data is only synced to the disk controller, not
+    -- to the platter, so there is still a slight risk of data
+    -- being lost due to a power failure.
+    --
+    -- [key]: sync
+    --
+    -- [type]: boolean
+    --
+    -- [default]: false
+    --
+    -- [note]: available to 'pool_change_options'
+  , pcoSync         :: Maybe Bool
+    -- | When true, Plasma uses @flock()@ for locking operations.
+    -- When false, Plasma uses System V semaphores.  The default is
+    -- to use semaphores on Linux, and @flock()@ on macOS.  Windows
+    -- always uses Windows mutexes, and is not affected by this
+    -- option.
+    --
+    -- [key]: flock
+    --
+    -- [type]: boolean
+    --
+    -- [default]: Linux: false, macOS: true
+  , pcoFlock        :: Maybe Bool
+    -- | When true, a checksum is computed for each protein, and
+    -- stored in the pool.  This should make it easier to detect
+    -- corruption, at the expense of some performance.
+    --
+    -- [key]: checksum
+    --
+    -- [type]: boolean
+    --
+    -- [default]: false
+  , pcoChecksum     :: Maybe Bool
+    -- | The UNIX permissions for this pool.  May be specified
+    -- either as an int32, or as a string which is parsed as an
+    -- octal number.  We recommend only using “7” or “0” in
+    -- each position, since write permission is needed to read from
+    -- pools, and vice versa, so it isn't really possible to
+    -- specify read and write permissions separately.  This option
+    -- has no effect on Windows.
+    --
+    -- [key]: mode
+    --
+    -- [type]: string (octal) or int32
+    --
+    -- [default]: -1
+  , pcoMode         :: Maybe StrOrInt
+    -- | The user which should own this pool.  Maybe be specified
+    -- either as a numeric uid, or as a string which is looked up
+    -- in the password file.  This option has no effect on Windows.
+    --
+    -- [key]: owner
+    --
+    -- [type]: string (username) or int32 (uid)
+    --
+    -- [default]: -1
+  , pcoOwner        :: Maybe StrOrInt
+    -- | The group which should own this pool.  Maybe be specified
+    -- either as a numeric gid, or as a string which is looked up
+    -- in the group file.  This option has no effect on Windows.
+    --
+    -- [key]: group
+    --
+    -- [type]: string (groupname) or int32 (gid)
+    --
+    -- [default]: -1
+  , pcoGroup        :: Maybe StrOrInt
+} deriving (Eq, Ord, Show, Read, Generic, NFData, Hashable)
 
 instance Default PoolCreateOptions where
   def = PoolCreateOptions
-        { pcoResizable    = True
-        , pcoSingleFile   = False
-        , pcoSize         = defPoolSize
-        , pcoTocCapacity  = 0
-        , pcoStopWhenFull = False
-        , pcoFrozen       = False
-        , pcoAutoDispose  = False
-        , pcoSync         = False
-        , pcoFlock        = defFlock
-        , pcoChecksum     = False
-        , pcoMode         = NoValue
-        , pcoOwner        = NoValue
-        , pcoGroup        = NoValue
+        { pcoResizable    = Nothing
+        , pcoSingleFile   = Nothing
+        , pcoSize         = Nothing
+        , pcoTocCapacity  = Nothing
+        , pcoStopWhenFull = Nothing
+        , pcoFrozen       = Nothing
+        , pcoAutoDispose  = Nothing
+        , pcoSync         = Nothing
+        , pcoFlock        = Nothing
+        , pcoChecksum     = Nothing
+        , pcoMode         = Nothing
+        , pcoOwner        = Nothing
+        , pcoGroup        = Nothing
         }
-    where
-      defPoolSize = 1024 * 1024    -- one megabyte
-      defFlock    = os == "darwin"
 
 instance Nameable PoolCreateOptions where
   typeName _ = "PoolCreateOptions"
@@ -215,8 +367,8 @@ poolCreateOptions :: Options PoolCreateOptions
 poolCreateOptions =
   [ FIELD("resizable",      pcoResizable   )
   , FIELD("single-file",    pcoSingleFile  )
-  , CFELD("size",           pcoSize        , tu64, ŝm)
-  , CFELD("toc-capacity",   pcoTocCapacity , tu64, ŝm)
+  , NFELD("size",           pcoSize        , NumUnt64)
+  , NFELD("toc-capacity",   pcoTocCapacity , NumUnt64)
   , FIELD("stop-when-full", pcoStopWhenFull)
   , FIELD("frozen",         pcoFrozen      )
   , FIELD("auto-dispose",   pcoAutoDispose )
@@ -227,6 +379,3 @@ poolCreateOptions =
   , FIELD("owner",          pcoOwner       )
   , FIELD("group",          pcoGroup       )
   ]
-
-tu64 :: Natural -> Slaw
-tu64 = preferNumeric NumInt64 . toInteger
