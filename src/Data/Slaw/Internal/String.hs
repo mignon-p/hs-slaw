0b42ad4e0e24e17305c9e2188c3eb2b5dff00387
Mignon Pelletier <code@funwithsoftware.org>
2024-08-30 14:48:30 -0700

    Remove functions for marshaling lazy ByteString.
    (These have been moved to the "plasma" package, instead.)

--- src/Data/Slaw/Internal/String.hs
+++ src/Data/Slaw/Internal/String.hs
@@ -15,19 +15,16 @@ module Data.Slaw.Internal.String
   , TextClass(..)
   , ByteStringClass(..)
   , indentLines
-  , withLazyByteStringAsCString
-  , withLazyByteStringAsCStringNL
-  , withLazyByteStringAsCStringLen
   ) where
 
 import Control.DeepSeq
 -- import Control.Exception
-import Control.Monad
+-- import Control.Monad
 import qualified Data.ByteString          as B
 import qualified Data.ByteString.Lazy     as L
 import qualified Data.ByteString.Short    as SBS
-import qualified Data.ByteString.Unsafe   as B
-import Data.Char
+-- import qualified Data.ByteString.Unsafe   as B
+-- import Data.Char
 -- import Data.Default.Class
 import Data.Hashable
 import Data.String
@@ -37,12 +34,12 @@ import qualified Data.Text.Encoding.Error as T
 import qualified Data.Text.Lazy           as LT
 import qualified Data.Text.Lazy.Encoding  as LT
 import Data.Word
-import Foreign.C.String
-import Foreign.C.Types
-import Foreign.Marshal.Alloc
-import Foreign.Marshal.Utils
-import Foreign.Ptr
-import Foreign.Storable
+-- import Foreign.C.String
+-- import Foreign.C.Types
+-- import Foreign.Marshal.Alloc
+-- import Foreign.Marshal.Utils
+-- import Foreign.Ptr
+-- import Foreign.Storable
 -- import GHC.Generics (Generic)
 
 import Data.Slaw.Internal.Util
@@ -170,64 +167,3 @@ indentLines indent str =
   in if trailingNewline
      then fromLazyText txt'
      else fromLazyText ("\n" `myStripSuffix` txt')
-
---
-
-withLazyByteStringAsCString :: L.ByteString
-                            -> (CString -> IO a)
-                            -> IO a
-withLazyByteStringAsCString lbs func =
-  withLazyByteStringAsCStringLen' False lbs (func . fst)
-
-withLazyByteStringAsCStringNL :: L.ByteString
-                              -> (CString -> IO a)
-                              -> IO a
-withLazyByteStringAsCStringNL lbs func =
-  withLazyByteStringAsCStringLen' True lbs (func . fst)
-
-withLazyByteStringAsCStringLen :: L.ByteString
-                               -> (CStringLen -> IO a)
-                               -> IO a
-withLazyByteStringAsCStringLen = withLazyByteStringAsCStringLen' False
-
-withLazyByteStringAsCStringLen' :: Bool -- add a terminating newline?
-                                -> L.ByteString
-                                -> (CStringLen -> IO a)
-                                -> IO a
-withLazyByteStringAsCStringLen' termNL lbs func = do
-  let len    = L.length lbs
-      extra  = 2 -- one for terminating NUL and one for optional newline
-      bufLen = len + extra
-  when (bufLen >= fromIntegral (maxBound :: Int)) $
-    fail $ "length " ++ show bufLen ++ " is too big"
-  allocaBytes (fromIntegral bufLen) $ \buf -> do
-    withLbs1 termNL (L.toChunks lbs) func buf 0
-
-withLbs1 :: Bool
-         -> [B.ByteString]
-         -> (CStringLen -> IO a)
-         -> CString
-         -> Int
-         -> IO a
-withLbs1 termNL [] func buf !pos = do
-  haveNL <- endsWithNewline buf pos
-  let needNL = termNL && not haveNL
-      byte1  = if needNL then newline else 0
-      byte2  = 0 :: CChar
-      len    = if needNL then pos + 1 else pos
-  pokeByteOff buf  pos      byte1
-  pokeByteOff buf (pos + 1) byte2
-  func (buf, len)
-withLbs1 termNL (bs:rest) func buf !pos = do
-  B.unsafeUseAsCStringLen bs $ \(src, srcLen) -> do
-    copyBytes (buf `plusPtr` pos) src srcLen
-    withLbs1 termNL rest func buf (pos + srcLen)
-
-newline :: CChar
-newline = (fromIntegral . ord) '\n'
-
-endsWithNewline :: CString -> Int -> IO Bool
-endsWithNewline _ 0 = return False
-endsWithNewline buf pos = do
-  lastByte <- peekByteOff buf (pos - 1)
-  return (lastByte == newline)
